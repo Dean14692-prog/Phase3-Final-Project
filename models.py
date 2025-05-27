efb69d3d12e2ad4cc2ddcbafd02463415dd790f6
Dean14692-prog <dennismuasyangui@gmail.com>
2025-05-27 15:13:11 +0300

    User class

--- models.py
+++ models.py
@@ -2,3 +2,15 @@ from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from database import Base
 from datetime import datetime
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String)
+    email = Column(String)
+    password_hash = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    notes = relationship('Note', back_populates='user')
+    complaints = relationship('Complaint', back_populates='user')
