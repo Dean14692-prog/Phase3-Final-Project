ffc982ca0a6c8334d657f62122899d059a372dfc
Dean14692-prog <dennismuasyangui@gmail.com>
2025-05-27 15:10:48 +0300

    Database.py

--- database.py
+++ database.py
@@ -3,8 +3,5 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///database.db', echo=True)
-
-
 SessionLocal = sessionmaker(bind=engine)
-
 Base = declarative_base()
